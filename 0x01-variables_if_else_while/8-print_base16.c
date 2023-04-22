c5391d4e786ee18f818ac2455f0a14ad5d236550
NomvulaHM <nomvulampaka7@gmail.com>
2023-04-22 07:25:35 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,11 +8,11 @@ int main(void)
 	int n;
 	int m;
 
-	for (n = 48; n <= 57; n++)
+	for (n = 48; n <= 58; n++)
 	{
 		putchar(n);
 	}
-	for (m = 97; m <= 102; n++)
+	for (m = 97; m <= 103; n++)
 	{
 		putchar(m);
 	}
