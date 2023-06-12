229d32b44825042ad97eac453e262cc1a0309ee5
NomvulaHM <nomvulampaka7@gmail.com>
2023-06-12 13:37:48 -0700

    Task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ * 0 when function fails or filename is NULL.
+ */
+size_t (size_t)
+={
+	char *buf;
+
+	if ("fd" -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	free(buf);
+	return (0);
+}
