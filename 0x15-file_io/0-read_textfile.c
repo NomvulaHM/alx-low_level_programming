eb34219d59136913baa5eee3c268cb3b2dcb99dd
NomvulaHM <nomvulampaka7@gmail.com>
2023-06-13 01:51:17 -0700

    Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,20 +1,36 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * read_textfile- Read text file print to STDOUT.
- * @filename: text file being read
- * @letters: number of letters to be read
- * Return: w- actual number of bytes read and printed
- * 0 when function fails or filename is NULL.
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
  */
-size_t (size_t)
-={
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t nrd, nwr;
 	char *buf;
 
-	if ("fd" -1)
+	if (!filename)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
+	close(fd);
+
 	free(buf);
-	return (0);
+
+	return (nwr);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,8 +1,13 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stddef.h>
+#ifndef _MAIN_
+#define _MAIN_
 
-size_t read_textfile(const char *filename, size_t letters);
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
